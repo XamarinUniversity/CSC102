812005580a4d5bb91b2e528a432695ed90f04328
Adam Patridge <patridge@gmail.com>
2017-09-05 11:36:15 -0600

    Initial course exercises import.

---
+++ Exercise 1/Completed/Parsing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Parsing
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//
+			// Prompt the user to enter their age. This uses Console.Write instead of
+			// Console.WriteLine so the cursor stays on the same line as the prompt.
+			//
+			Console.Write("Please enter your age: ");
+
+			//
+			// Read the age. The user's input arrives in the program as a string,
+			// e.g. the input will be something like the sequence of characters "45".
+			//
+			string yourAge = Console.ReadLine();
+
+			//
+			// Convert the age from string to int.
+			//
+			int yourAgeAsInt = int.Parse(yourAge);
+
+			//
+			// Output the age.
+			//
+			Console.WriteLine("Your age is {0}", yourAge.ToString());
+		}
+	}
+}
---
+++ Exercise 2/Completed/FizzBin/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBin
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//
+			// Loop 100 times, from 1 to 100 inclusive.
+			//
+			for (int i = 1; i <= 100; i++)
+			{
+				if (i % 3 == 0 && i % 5 == 0) // test if i is divisible by both 3 and 5
+				{
+					Console.WriteLine("FizzBin!");
+				}
+				else if (i % 3 == 0) // test if i is divisible by 3
+				{
+					Console.WriteLine("Fizz");
+				}
+				else if (i % 5 == 0) // test if i is divisible by 5
+				{
+					Console.WriteLine("Bin");
+				}
+				else
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
