de1257d40b54830b01289f348d7096c89bbeef92
Adam Patridge <patridge@gmail.com>
2017-09-21 11:00:26 -0600

    Add files missed by migration.

---
+++ Homework/Completed/Arithmetic/Arithmetic/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Arithmetic
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string temp;
+
+			//
+			// User input
+			//
+			Console.Write("Please enter the first number: ");
+			temp = Console.ReadLine();
+			int x = int.Parse(temp);
+
+			Console.Write("Please enter the second number: ");
+			temp = Console.ReadLine();
+			int y = int.Parse(temp);
+
+			//
+			// Addition
+			//
+			int sum = x + y;
+			Console.WriteLine("{0} + {1} = {2}", x, y, sum);
+
+			//
+			// Subtraction
+			//
+			int difference = x - y;
+			Console.WriteLine("{0} - {1} = {2}", x, y, difference);
+
+			//
+			// Multiplication
+			//
+			int product = x * y;
+			Console.WriteLine("{0} * {1} = {2}", x, y, product);
+
+			//
+			// Division
+			//
+			if (y == 0)
+			{
+				Console.WriteLine("Error: denominator is zero");
+			}
+			else
+			{
+				int quotient = x / y;
+				Console.WriteLine("{0} / {1} = {2}", x, y, quotient);
+			}
+		}
+	}
+}
